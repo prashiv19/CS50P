4c26c7364b1e0e2d55121abd7e24017b30c90e64
prashiv19 <prashivpandit@icloud.com>
2026-07-30 22:10:15 +0100

    Some revision on basic python programming

---
+++ hello.py
@@ -0,0 +1,16 @@
+# Ask user for their name
+name = input("What's your name?")
+#Say hello to the user
+print("Hello,", name)
+
+#remove the new line at the end of print
+print("Hello, ", end="")
+print(name)
+
+#print quotation marks
+
+print("Hello 'Friend'")
+print("Hello \"Friend\"")
+
+#Print Format strings
+print(f"Hello, {name}")
