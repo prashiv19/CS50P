09f68798b7f2e5c09cf524f72ba6f2c177c8a1da
prashiv19 <prashivpandit@icloud.com>
2026-08-01 16:52:17 +0100

    Split first name & last name

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 # Ask user for their name
 name = input("What's your name?").strip().title()
 
+#Splits user name into first name and last name
+
+first, last = name.split(" ")
+
 #Print Format strings
-print(f"Hello, {name}")
+print(f"Hello, {first}")
+
+
+
 
