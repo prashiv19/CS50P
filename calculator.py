56d0bda1d7dc73c03b402680edf093c2d6fc4afd
prashiv19 <prashivpandit@icloud.com>
2026-08-01 17:16:02 +0100

    Simple calculator-float

--- calculator.py
+++ calculator.py
@@ -1,4 +1,15 @@
-x = int(input("What is x? "))
-y = int(input("What is y? "))
+#Integers
 
-print(x + y)
+#x = int(input("What is x? "))
+#y = int(input("What is y? "))
+#print(x + y)
+
+#Float
+
+x = float(input("What is x? "))
+y = float(input("What is y? "))
+
+
+#round
+z= round(x + y)
+print(z)
