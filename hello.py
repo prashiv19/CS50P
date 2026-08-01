e5664c48c6ba955478f1a934418ad3a92987373f
prashiv19 <prashivpandit@icloud.com>
2026-08-01 16:44:37 +0100

    Some revision on basic python programming-shorter yet more effective

--- hello.py
+++ hello.py
@@ -1,25 +1,5 @@
 # Ask user for their name
-name = input("What's your name?")
-#Say hello to the user
-print("Hello,", name)
-
-#remove the new line at the end of print
-print("Hello, ", end="")
-print(name)
-
-#print quotation marks
-
-print("Hello 'Friend'")
-print("Hello \"Friend\"")
-
-#removes whitespace from string
-name = name.strip()
-
-#capitalises the users first name
-name = name.capitalize()
-
-#capitalises the users name
-name = name.title()
+name = input("What's your name?").strip().title()
 
 #Print Format strings
 print(f"Hello, {name}")
