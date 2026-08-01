b3c5c168b314da9e4739f30cc6232eff7ac2c87f
prashiv19 <prashivpandit@icloud.com>
2026-08-01 17:21:48 +0100

    Simple calculator-round to 2 digits & division

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 #Integers
 
-#x = int(input("What is x? "))
+#x = int(input("What is x? "))99
 #y = int(input("What is y? "))
 #print(x + y)
 
@@ -11,7 +11,12 @@ y = float(input("What is y? "))
 
 
 #round
-z = round(x + y)
+#z = round(x + y)
 
 #Specify commas in numbers
-print(f"{z:,}")
+#print(f"{z:,}")
+
+
+z = round(x / y, 2)
+
+print(z)
